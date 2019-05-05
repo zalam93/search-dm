2aabe0188456a3546effd7e179891051468e28b9
zalam93 <35038389+zalam93@users.noreply.github.com>
2019-05-04 22:39:27 -0500

    Update urls.py

--- django_classify/blog/urls.py
+++ django_classify/blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about'),
-    path('output/', views.output, name='blog-search')
+    path('home', views.home, name='blog-home'),
+    path('classify', views.classify, name='blog-classify')
+
+
 ]
